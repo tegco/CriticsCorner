f727f3c6caa25d9f0c3d817355294bb3ab1c4372
tegco <tegco@iscte-iul.pt>
2023-05-10 16:41:30 +0100

    inclui função para escrever na base de dados -

--- criticscorner/criticscorner/api.py
+++ criticscorner/criticscorner/api.py
@@ -1,31 +1,31 @@
 import os
 import django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "criticscorner.criticscorner.criticscorner.settings")
+import requests
+import sqlite3
 
+from django.conf.global_settings import SECRET_KEY
+from django.db import connection
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'criticscorner.settings')
 django.setup()
 
-import requests
-from django.conf import settings
 
-from criticscorner.criticscorner.criticscorner.settings import BASE_DIR, SECRET_KEY
+def write_to_db(data, table_name):
+    conn = sqlite3.connect("db.sqlite3")  # Define the data you want to insert
 
+    # Build the SQL query
+    columns = ", ".join(data.keys())
+    placeholders = ", ".join(["%s"] * len(data))
+    values = list(data.values())
+    query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
 
-settings.configure(
-    INSTALLED_APPS=['review.apps.ReviewConfig', 'django.contrib.admin', 'django.contrib.auth',
-                    'django.contrib.contenttypes', 'django.contrib.sessions',
-                    'django.contrib.messages', 'django.contrib.staticfiles', ],
-    DATABASES={
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "criticscorner.settings")
+    # Execute the query
+    with connection.cursor() as cursor:
+        cursor.execute(query, values)
+        connection.close()
 
 
 def fetch_movies_from_api():
-
     url = "https://online-movie-database.p.rapidapi.com/title/get-top-rated-movies"
 
     headers = {
@@ -56,6 +56,7 @@ def fetch_movies_details_and_populate_database(imdb_id):
     movie = response.json().get("results")[0]
 
     top_crew = fetch_top_crew(imdb_id)
+
     print("\t\tFilme com o nome " + movie.get("title")
           + "\n\t\t do ano " + str(movie.get("year"))
           + "\n\t\t com o poster " + movie.get("image").get("url")
@@ -65,72 +66,65 @@ def fetch_movies_details_and_populate_database(imdb_id):
           + "\n\t\t com os autores " + str(top_crew[1])
           + "\n\t\t com os generos " + str(fetch_genres(imdb_id)))
 
-    from criticscorner.criticscorner.review.models import Movie
-    movie = Movie(
-        imdb_id=imdb_id,
-        title= movie.get("title"),
-        year= int(movie.get("year")),
-        poster_url= movie.get("image").get("url"),
-        plot= fetch_plot(imdb_id),
-        director=str(top_crew[0]),
-        writer=str(top_crew[1]),
-        duration= movie.get("runningTimeInMinutes"),
-        genres=str(fetch_genres(imdb_id))
-    )
-    movie.save()
-    print(f"Saved movie {movie.title} to the database")
+    data = {
+        "imdb_id": imdb_id,
+        "title": movie.get("title"),
+        "year": int(movie.get("year")),
+        "poster_url": movie.get("image").get("url"),
+        "plot": fetch_plot(imdb_id),
+        "director": str(top_crew[0]),
+        "writer": str(top_crew[1]),
+        "duration": movie.get("runningTimeInMinutes"),
+        "genres": str(fetch_genres(imdb_id))
+    }
+
+    write_to_db(data, "review_movie")
+
 
 def fetch_plot(imdb_id):
     url = "https://online-movie-database.p.rapidapi.com/title/get-plots"
-
     querystring = {"tconst": imdb_id}
-
     headers = {
         "X-RapidAPI-Key": SECRET_KEY,
         "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
     }
-
     response = requests.get(url, headers=headers, params=querystring)
+
     return response.json().get("plots")[0].get("text")
 
 
-
 def fetch_top_crew(imdb_id):
-
     url = "https://online-movie-database.p.rapidapi.com/title/get-top-crew"
-
     querystring = {"tconst": imdb_id}
 
     headers = {
         "X-RapidAPI-Key": SECRET_KEY,
         "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
     }
-
     response = requests.get(url, headers=headers, params=querystring)
 
-
-    dir_array = []
+    diretors = []
     for director in response.json().get("directors"):
-        dir_array.append(director.get("name"))
+        diretors.append(director.get("name"))
 
-    wri_array = []
+    writters = []
     for writer in response.json().get("writers"):
-        wri_array.append(writer.get("name"))
+        writters.append(writer.get("name"))
 
-    return [dir_array, wri_array]
+    return [diretors, writters]
 
 
 def fetch_genres(imdb_id):
     url = "https://online-movie-database.p.rapidapi.com/title/get-overview-details"
-
     querystring = {"tconst": imdb_id}
 
     headers = {
         "X-RapidAPI-Key": SECRET_KEY,
         "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
     }
-
     response = requests.get(url, headers=headers, params=querystring)
+
     return response.json().get("genres")
+
 
 fetch_movies_from_api()
