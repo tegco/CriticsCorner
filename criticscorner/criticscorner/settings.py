b8eefbb479fddd1b96f35567c18390b1ecc7d451
tegco <tegco@iscte-iul.pt>
2023-05-13 23:46:38 +0100

    Update settings.py

--- criticscorner/criticscorner/settings.py
+++ criticscorner/criticscorner/settings.py
@@ -131,3 +131,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/votacao/static/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'votacao/static/media')
