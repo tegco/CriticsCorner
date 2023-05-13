da6c883abd54aee5678fe79944c492cdd929b9dd
Diogo Carvalho <djcos1@iscte-iul.pt>
2023-05-13 23:52:15 +0100

    settings media

--- criticscorner/criticscorner/settings.py
+++ criticscorner/criticscorner/settings.py
@@ -131,3 +131,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/review/static/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'review/static/media')
